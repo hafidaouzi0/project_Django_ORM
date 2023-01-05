319aaccf3381944a988f80ab126e4dedfaade959
hafidaouzi0 <faouzibrahim2014@gmail.com>
2023-01-05 22:09:01 +0100

    test commit

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Brand,Category,Product
+from .models import Brand,Category,Product,ProductType
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Brand)
+admin.site.register(ProductType)
+
--- inventory/models.py
+++ inventory/models.py
@@ -1,24 +1,50 @@
 from django.utils import timezone
 from django.db import models
 
-# Create your models here.
-class Product(models.Model):
-    name=models.CharField(max_length=50)
-    qty=models.IntegerField()
-    price=models.DecimalField(max_digits=7,decimal_places=2,null=True)
-    #date automatically generated when a product is created
-    date_added=models.DateTimeField(default=timezone.now)
-    #date automatically updated when a product is updated
-    date_updated=models.DateTimeField(auto_now=True,null=True)
-    url=models.SlugField(null=True)
-    is_active=models.BooleanField(default=True)
+
+
+
 
 class Brand(models.Model):
-    brand_id=models.IntegerField(primary_key=True)
+
     name=models.CharField(max_length=50)
 
     def __str__(self):
       return self.name
 
+
+
+class ProductType(models.Model):
+    name=models.CharField(max_length=255,unique=True)
+
+    def __str__(self):
+      return self.name
+
+#a category can have one or many products
 class Category(models.Model):
     name=models.CharField(max_length=100)
+     
+    def __str__(self):
+      return self.name
+
+
+# a product can have one and only category
+class Product(models.Model):
+   
+
+    name=models.CharField(max_length=50)
+    qty=models.IntegerField()
+    price=models.DecimalField(max_digits=7,decimal_places=2,null=True)
+    #date automatically generated when a product is created
+    date_added=models.DateTimeField(auto_now_add=True,editable=False)
+    #date automatically updated when a product is updated
+    date_updated=models.DateTimeField(auto_now=True,null=True)
+    url=models.SlugField(null=True)
+    is_active=models.BooleanField(default=True)
+    description=models.TextField(blank=True)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+
+
+class Stock(models.Model):
+  units=models.BigIntegerField()
+  product=models.OneToOneField(Product,on_delete=models.CASCADE)
